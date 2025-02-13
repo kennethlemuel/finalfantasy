d7f5bd0d3945732fa52698adedbec297bfa222cc
kenneth <kennethlemuel05@gmail.com>
2025-02-13 22:35:10 +0800

    added dialogues

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    //overlay and volume code
     const overlay = document.getElementById('overlay');
     const overlaytext = document.getElementById('overlaytext');
     const audioEl = document.getElementById('bg');
@@ -17,7 +19,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
         overlay.addEventListener('transitionend', function() {
             overlay.style.display = 'none';
+            const dialogueBox = document.getElementById("dialogueBox");
+            dialogueBox.style.display = 'block';
       }, {once: true});
+    });
+
+
+    //dialogue code
+    const dialogues = [
+        "Hey there Elysia, I'm Aerith!",
+        "Ken told me a lot about you...",
+        "Hm... especially about your beautiful soul.",
+        "He must really love you, doesn't he!",
+        "It... seems like he wants to ask you something...",
+    ];
+
+    let currentdialogue = 0;
+    const dialogueBox = document.getElementById("dialogueBox");
+    const dialogueText = document.getElementById("dialogueText");
+
+    dialogueBox.addEventListener("click", function(){
+        currentdialogue = (currentdialogue + 1) % dialogues.length;
+        dialogueText.textContent = dialogues[currentdialogue];
     });
 });
   
